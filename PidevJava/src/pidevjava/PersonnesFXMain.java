ea7cbeac6851fcc0ca8f13aa2b78f8220f8e03d7
eya1003 <eya.amor@esprit.tn>
2022-04-22 02:40:57 +0100

    front

--- PidevJava/src/pidevjava/PersonnesFXMain.java
+++ PidevJava/src/pidevjava/PersonnesFXMain.java
@@ -39,7 +39,7 @@ public class PersonnesFXMain extends Application {
     public void start(Stage primaryStage) {
        this.stage = primaryStage;
         try {
-            parent = FXMLLoader.load(getClass().getResource("/gui/ListEmplacements.fxml"));
+            parent = FXMLLoader.load(getClass().getResource("/gui/MenuFXML.fxml"));
             Scene scene = new Scene(parent);
           
             stage.setTitle("Acceuil");
